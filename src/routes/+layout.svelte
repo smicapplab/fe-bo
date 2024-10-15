153aa828d97e70f37be7f5a88b019435212f52c0
Steve Torrefranca <92366134+smicapplab@users.noreply.github.com>
2024-10-15 23:39:37 +0800

    Spinner loading

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,24 @@
 <script>
+	import { afterNavigate, beforeNavigate } from '$app/navigation';
+	import { Icons } from '$lib/components/ui/icons';
 	import { Toaster } from '$lib/components/ui/sonner';
+	import { isLoading } from '$lib/stores/loading';
 	import '../app.css';
+
+	beforeNavigate(() => {
+		isLoading.set(true); // Show loading animation before navigation
+	});
+
+	afterNavigate(() => {
+		isLoading.set(false); // Hide loading animation after navigation
+	});
 </script>
 
+{#if $isLoading}
+	<div class="flex h-screen items-center justify-center">
+		<Icons.spinner class="h-20 w-20 animate-spin" />
+	</div>
+{/if}
+
 <Toaster />
-<slot></slot>
+<slot />
